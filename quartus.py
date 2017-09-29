e8857c1be90dd17ce45bf05e57ccca26a2808207
dymnz <vicodin1123@gmail.com>
2017-09-30 01:59:37 +0800

    'hmm'

---
+++ quartus.py
@@ -0,0 +1,77 @@
+import pyautogui
+import time
+
+from window_manager import window_manager
+
+def translate_to_center_cord(x, y, w, h):
+	# For some reason the y-cord needs to be compensated
+	#return (x + w/2 , y + h/2 - (image_h - screen_h))
+	return (x, y)
+
+def start_compile():
+	result = pyautogui.locate('compile_button.png', image)
+	if result is None:
+		print("Cannot start compiling")
+		exit()
+	(x, y, w, h) = result
+	(center_x, center_y) = translate_to_center_cord(x, y, w, h)
+	pyautogui.click(center_x, center_y)
+
+def is_compile_success():
+	result = pyautogui.locate('compile_complete.png', image)
+	if result is not None:
+		print(result)
+		return True
+	return False
+
+def is_compile_failed():
+	result = pyautogui.locate('compile_error.png', image)
+	if result is not None:
+		print(result)
+		return True
+	return False
+
+WM = window_manager()
+
+(image, image_w, image_h) = WM.grab_window_image('Quartus')
+(screen_w, screen_h) = pyautogui.size()
+
+print(screen_w, screen_h, image_w, image_h)
+
+if image is None:
+	print('Cannot find Quartus')
+	exit()
+
+WM.bring_window_to_front('Quartus')
+
+start_compile()
+
+WM.minimize_window('Quartus')
+
+# Wait for the compiler to start
+time.sleep(5)
+timeout = time.time() + 60*5
+
+while True:
+	(image, image_w, image_h) = WM.grab_window_image('Quartus')
+	if abs(image_w-screen_w) > 200:
+		print("Capture size error")		
+		break
+
+	if time.time() > timeout:
+		print("Timeout")
+		break
+
+	success = is_compile_success()
+	if success:
+		print("Compile Success")
+		break
+	failed = is_compile_failed()
+	if failed:
+		print("Compile Error")
+		break
+	time.sleep(1)
+
+
+WM.bring_window_to_front('Quartus')
+WM.beep(450, 500)
---
+++ window_manager.py
@@ -0,0 +1,79 @@
+# https://stackoverflow.com/questions/19695214/python-screenshot-of-inactive-window-printwindow-win32gui/24352388#24352388
+import win32gui
+import win32ui
+import winsound
+import re
+import time
+from ctypes import windll
+from PIL import Image
+
+class window_manager:
+	_hwnd = None
+	origin_left = None
+	origin_top = None
+
+	def _enumHandler(self, hwnd, window_name):
+		if window_name in win32gui.GetWindowText(hwnd):
+			self._hwnd = hwnd
+			print('Found: {}'.format(win32gui.GetWindowText(self._hwnd)))
+
+	def bring_window_to_front(self, window_name):     
+		win32gui.EnumWindows(self._enumHandler, window_name)    
+		win32gui.SetWindowPos(self._hwnd, 0, 0, 0, 0, 0, 1)  
+		win32gui.ShowWindow(self._hwnd, 3)
+		win32gui.SetForegroundWindow(self._hwnd)
+
+	def minimize_window(self, window_name):
+		win32gui.EnumWindows(self._enumHandler, window_name)   
+		win32gui.ShowWindow(self._hwnd, 2)
+
+	def beep(self, frequency, duration):
+		winsound.Beep(frequency, duration)
+
+	def grab_window_image(self, window_name):
+		win32gui.EnumWindows(self._enumHandler, window_name)		
+		win32gui.ShowWindow(self._hwnd, 3)
+
+		# Change the line below depending on whether you want the whole window
+		# or just the client area. 
+		#left, top, right, bot = win32gui.GetClientRect(hwnd)
+		left, top, right, bot = win32gui.GetWindowRect(self._hwnd)
+		w = right - left
+		h = bot - top
+
+		self.origin_left = left
+		self.origin_top = top
+		# Workaround - activate the window at the bottom
+		win32gui.SetWindowPos(self._hwnd, 1, 0, 0, 0, 0, 1)		
+
+		hwndDC = win32gui.GetWindowDC(self._hwnd)
+		mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+		saveDC = mfcDC.CreateCompatibleDC()
+
+		saveBitMap = win32ui.CreateBitmap()
+		saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+		saveDC.SelectObject(saveBitMap)
+
+		# Change the line below depending on whether you want the whole window
+		# or just the client area. 
+		#result = windll.user32.PrintWindow(self._hwnd, saveDC.GetSafeHdc(), 1)
+		result = windll.user32.PrintWindow(self._hwnd, saveDC.GetSafeHdc(), 0)
+
+		bmpinfo = saveBitMap.GetInfo()
+		bmpstr = saveBitMap.GetBitmapBits(True)
+
+		im = Image.frombuffer(
+			'RGB',
+			(bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+			bmpstr, 'raw', 'BGRX', 0, 1)
+
+		win32gui.DeleteObject(saveBitMap.GetHandle())
+		saveDC.DeleteDC()
+		mfcDC.DeleteDC()
+		win32gui.ReleaseDC(self._hwnd, hwndDC)
+
+		# Workaround - move the window back to original position
+		win32gui.SetWindowPos(self._hwnd, 1, 0, 0, 0, 0, 1)
+		#win32gui.SetWindowPos(self._hwnd, 1, self.origin_top, self.origin_left, 0, 0, 1)  
+		return (im, bmpinfo['bmWidth'], bmpinfo['bmHeight'])
